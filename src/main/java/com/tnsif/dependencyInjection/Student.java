3991b26cf0e1c364b57a63cf802ffe3b0a1c6a5f
Spoorthi G <spoorthiggowda917@gmai.com>
2025-12-02 19:42:01 +0530

    First commit

--- src/main/java/com/tnsif/dependencyInjection/Student.java
+++ src/main/java/com/tnsif/dependencyInjection/Student.java
@@ -5,23 +5,29 @@ public class Student {
     private int id;
     private String name;
 
-    // Getter and Setter for id
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+//   // Getter and Setter for id
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    // Getter and Setter for name
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    // Getter and Setter for name
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Display method
+    
+    public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	// Display method
     public void display() {
         System.out.println("Student ID: " + id);
         System.out.println("Student Name: " + name);
